88979cbf2020486eee3adef2538868b37d14d26f
snowflake-warzywko <120725668+snowflake-warzywko@users.noreply.github.com>
2022-12-19 15:35:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,4 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.header("Fruityvice Fruit Advice!")
+streamlit.dataframe(fruityvice_normalized)
