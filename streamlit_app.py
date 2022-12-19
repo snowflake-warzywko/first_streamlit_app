97de6a599a997d91bd964da8790ca39091fb646a
snowflake-warzywko <120725668+snowflake-warzywko@users.noreply.github.com>
2022-12-19 17:05:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ except URLError as e:
 
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("select * from fruit_load_list")
+    my_cur.execute("select * from pc_rivery_db.fruit_load_list")
     return my_cur.fetchall()
 
 if streamlit.button('Get Fruit Load List'):
